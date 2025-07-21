fb07abcb5007d1a8cd44aa7c614bfd97745e30a3
Pankaj Yadav <134844846+theopendraft@users.noreply.github.com>
2025-07-21 15:55:07 +0530

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -8,12 +8,12 @@ import Contact from "./pages/Contact";
 export default function App() {
   return (
     <Layout>
-      <Routes>
+      <HashRouter>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
         <Route path="/contact" element={<Contact />} />
-      </Routes>
+      </HashRouter>
     </Layout>
   );
 }
