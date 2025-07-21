ec759223f69d581472d5ad8a296ae04813cadef9
Pankaj Yadav <134844846+theopendraft@users.noreply.github.com>
2025-07-21 16:15:11 +0530

    Updates

--- assets/react-B4vmFfpS.js
+++ assets/react-B4vmFfpS.js
@@ -1,4 +1,4 @@
-import{R as I}from"./index-DsBe8_78.js";import{C as x}from"./custom-media-element-Ce0vsE-t.js";import{M as A,H as m}from"./hls-D4QvIzvx.js";const H=e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{const{src:h,...E}=t;return`
+import{R as I}from"./index-F2GTdpTt.js";import{C as x}from"./custom-media-element-Ce0vsE-t.js";import{M as A,H as m}from"./hls-D4QvIzvx.js";const H=e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{const{src:h,...E}=t;return`
         <script type="application/json" id="config">
           ${JSON.stringify(n.config||{})}
         <\/script>
