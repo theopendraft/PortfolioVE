4ffd6c14589393fb999f3775964a6a2300335f2f
Pankaj Yadav <134844846+theopendraft@users.noreply.github.com>
2025-07-21 16:14:25 +0530

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,17 +7,14 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <Layout>
-    <HashRouter>
     
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    
-    </HashRouter>
     </Layout>
   );
 }
