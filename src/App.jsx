ea7f0bacc093900e073a900aa2da9a220cdf9eb8
Pankaj Yadav <134844846+theopendraft@users.noreply.github.com>
2025-07-21 15:56:38 +0530

    HashRouter-2

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
+    <HashRouter>
     <Layout>
-      <HashRouter>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
         <Route path="/contact" element={<Contact />} />
-      </HashRouter>
+      </Routes>
     </Layout>
+    </HashRouter>
   );
 }
